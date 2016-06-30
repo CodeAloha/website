3188c158641c88860819a11bf4cca5ef51b62aaa
Chris Wheeler <chris.wheeler@hightail.com>
2016-06-29 17:09:04 -0700

    Automate js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,27 @@
 var gulp   = require('gulp'),
     watch  = require('gulp-watch'),
     sass   = require('gulp-sass'),
-    uglify = require('gulp-uglifycss'),
-    minify = require('gulp-clean-css'),
-    concat = require('gulp-concat-css');
+    minify = require('gulp-minify'),
+    minifycss = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 gulp.task('sass', function () {
     gulp.src('./css/sass/main.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./js/dist/*.js')
         .pipe(uglify())
         .pipe(minify())
-        .pipe(gulp.dest('./css'));
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./js'));
 });
 
 gulp.task('run', function () {
     // Endless stream mode
-    gulp.watch('./css/sass/*.scss', ['sass']);
+    gulp.watch([ './css/sass/*.scss', './js/dist/*.js' ], ['sass', 'js']);
 });
