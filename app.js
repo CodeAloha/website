0dce96f5a53e50a3b061b9409631e7a595fec38b
Chris Wheeler <chris.wheeler@hightail.com>
2016-07-18 16:00:00 -0700

    force domain was a bad idea.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-var forceDomain    = require('node-force-domain');
 
 // configuration ===========================================
 
@@ -29,10 +28,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(forceDomain({
-    hostname: 'www.javacup.io'
-}));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override'));
