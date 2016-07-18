3bedebe17188f700b0f716cf7f2ceefeb44575cf
Chris Wheeler <chris.wheeler@hightail.com>
2016-07-18 15:19:58 -0700

    Port 3002

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override');
 
 
 // set our port
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3002;
 
 var router = express.Router({
     caseSensitive: app.get('case sensitive routing'),
