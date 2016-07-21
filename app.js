0d40211a8b22df48d4498097106122564c4e25d5
Chris Wheeler <chris.wheeler@hightail.com>
2016-07-20 23:45:11 -0700

    Add https

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express        = require('express');
 var app            = express();
 var fs             = require('fs');
 var bodyParser     = require('body-parser');
+var https          = require('https');
 var methodOverride = require('method-override');
 
 
